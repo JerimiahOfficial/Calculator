c8cfc6766cf1cb8f43241f54b1a482149341854f
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2021-10-06 18:58:56 -0400

    Update CalculatorApp.cpp

--- CalculatorApp/CalculatorApp.cpp
+++ CalculatorApp/CalculatorApp.cpp
@@ -1,153 +1,75 @@
-#include <string>
-#include <sstream>
-#include <vector>
+#include "Operators.h"
+#include "Menu.h"
 #include <iostream>
-#include <iterator>
-#include <iomanip>
-#include <valarray>
-#include <algorithm>
-#include <math.h>
+#include <sstream>
 
-int iresult;
-float fresult;
+namespace calculator {
 
-enum class Operators {
-    addition,
-    division,
-    exponent,
-    multiplication,
-    subtraction,
-    modulo,
-};
-
-constexpr uint32_t hash(const char* data, size_t const size) noexcept {
-    uint32_t hash = 5381;
-
-    for (const char* c = data; c < data + size; ++c)
-        hash = ((hash << 5) + hash) + (unsigned char)*c;
-
-    return hash;
-}
-
-Operators hashit(std::string const& inString) {
-    if (inString == "add") 
-        return Operators::addition;
-
-    if (inString == "div")
-        return Operators::division;
-
-    if (inString == "exp")
-        return Operators::exponent;
-
-    if (inString == "mul")
-        return Operators::multiplication;
-
-    if (inString == "mod")
-        return Operators::modulo;
-
-    if (inString == "sub") 
-        return Operators::subtraction;
-}
-
-void drawMenu() {
-    std::string Menu[] = { 
-        "Calculator",               // 0
-        "\tadd - addition",         // 1
-        "\tdiv - division",         // 2
-        "\texp - exponent",         // 3
-        "\tmul - multiplication",   // 4
-        "\tmod - modulo",           // 5
-        "\tsub - subtraction",      // 6
+    enum class Operators {
+        add,
+        div,
+        exp,
+        mul,
+        sub,
+        mod,
     };
 
-    for (int i = 0; i < 6; i++)
-        std::cout << Menu[i] << std::endl;
+    constexpr uint32_t hash(const char* data, size_t const size) noexcept {
+        uint32_t hash = 5381;
+
+        for (const char* c = data; c < data + size; ++c)
+            hash = ((hash << 5) + hash) + (unsigned char)*c;
+
+        return hash;
+    }
+
+    Operators hashit(std::string const& inString) {
+        if (inString == "add")
+            return Operators::add;
+
+        if (inString == "div")
+            return Operators::div;
+
+        if (inString == "exp")
+            return Operators::exp;
+
+        if (inString == "mul")
+            return Operators::mul;
+
+        if (inString == "mod")
+            return Operators::mod;
+
+        if (inString == "sub")
+            return Operators::sub;
+    }
+
+    void calculator() {
+        Menu::drawMenu();
+
+        std::string Command;
+        std::getline(std::cin, Command);
+
+        std::istringstream iss(Command);
+        std::vector<std::string> arguments{ std::istream_iterator<std::string>(iss), {} };
+        std::vector<std::string> args = { arguments.begin() + 1, arguments.end() };
+
+        switch (hashit(arguments[0])) {
+            case Operators::add: Operations::add(args); break;
+            case Operators::div: Operations::div(args); break;
+            case Operators::exp: Operations::exp(args); break;
+            case Operators::mul: Operations::mul(args); break;
+            case Operators::mod: Operations::mod(args); break;
+            case Operators::sub: Operations::sub(args); break;
+            default: break;
+        }
+
+        for (int i = 0; i < 5; i++)
+            std::cout << std::endl;
+
+        calculator();
+    }
 }
 
 int main() {
-    drawMenu();
-
-    std::string Command;
-    std::getline(std::cin, Command);
-
-    std::istringstream iss(Command);
-    std::vector<std::string> arguments {std::istream_iterator<std::string>(iss), {}};
-
-    switch (hashit(arguments[0]))
-    {
-    case Operators::addition:
-        std::cout << "----- addition -----" << std::endl;
-        arguments.erase(arguments.begin());
-        
-        for (auto i : arguments)
-            fresult += std::stof(i);
-
-        std::cout << fresult << std::endl;
-        break;
-
-    case Operators::division:
-        std::cout << "----- division -----" << std::endl;
-        arguments.erase(arguments.begin());
-        fresult = std::stof(arguments[0]);
-
-        for (int i = 1; i < (int)arguments.size(); i++)
-            fresult /= std::stof(arguments[i]);
-
-        std::cout << fresult << std::endl;
-        break;
-
-    case Operators::exponent:
-        std::cout << "----- exponent -----" << std::endl;
-        arguments.erase(arguments.begin());
-        fresult = std::stof(arguments[0]);
-
-        for (int i = 1; i < (int)arguments.size(); i++)
-            fresult = std::pow(fresult, std::stof(arguments[i]));
-
-        std::cout << fresult << std::endl;
-        break;
-
-    case Operators::multiplication:
-        std::cout << "----- multiplication -----" << std::endl;
-        arguments.erase(arguments.begin());
-        fresult = std::stof(arguments[0]);
-
-        for (int i = 1; i < (int)arguments.size(); i++)
-            fresult *= std::stof(arguments[i]);
-
-        std::cout << fresult << std::endl;
-        break;
-
-    case Operators::modulo:
-        std::cout << "----- modulo -----" << std::endl;
-        arguments.erase(arguments.begin());
-        iresult = std::stoi(arguments[0]);
-
-        for (int i = 1; i < (int)arguments.size(); i++)
-            iresult %= std::stoi(arguments[i]);
-
-        std::cout << iresult << std::endl;
-        break;
-
-    case Operators::subtraction:
-        std::cout << "----- subtraction -----" << std::endl;
-        arguments.erase(arguments.begin());
-
-        fresult = std::stof(arguments[0]);
-        for (int i = 1; i < (int)arguments.size(); i++)
-            fresult -= std::stof(arguments[i]);
-
-        std::cout << fresult << std::endl;
-        break;    
-
-    default:
-        break;
-    }
-
-    fresult, iresult = 0;
-
-    for (int i = 0; i < 5; i++)
-        std::cout << std::endl;
-    
-    main();
-}                                                                                                                                                             
+    calculator::calculator();
+}
