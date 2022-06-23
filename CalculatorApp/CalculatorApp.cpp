d831ebe5e722a2f08cca33f4acfc55826501885b
Gage Lodba <JerimiahOfficial@gmail.com>
2022-06-23 00:33:16 -0400

    change from runtime error to a simple "Invalid operator" message.

--- CalculatorApp/CalculatorApp.cpp
+++ CalculatorApp/CalculatorApp.cpp
@@ -12,6 +12,7 @@ namespace calculator {
 		sub,
 		mod,
 		quit,
+		none,
 	};
 
 	Commands hashit(std::string const& inString) {
@@ -36,7 +37,7 @@ namespace calculator {
 		if (inString == "quit")
 			return Commands::quit;
 
-		throw std::runtime_error("Unknown command");
+		return Commands::none;
 	}
 
 	void calculator() {
